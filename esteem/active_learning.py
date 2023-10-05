2b20b55f87a29e383aba4b4f2096c574b25fd160
Jacob Eller <msught@bethe.theory.warwick.ac.uk>
2023-10-05 22:37:33 +0100

    Spectra Edit

--- esteem/active_learning.py
+++ esteem/active_learning.py
@@ -430,21 +430,21 @@ def create_spectra_tasks(spectra_task,train_calcs,targets,rand_seed,meth,ntraj,c
             spectra_task.verbosity = 'normal'
             if spectra_task.wrapper is not None:
                 spectra_task.wrapper.task = spectra_task.mode.upper()
-                spectra_task.wrapper.rootname = f'{{solu}}_{{solv}}_{targstr}_spec'
-                spectra_task.wrapper.input_filename = f'{{solu}}_{{solv}}_{targstr}_spec_input'
+                spectra_task.wrapper.rootname = f'{{solu}}_{{solv}}_{targstr}_{spectra_task.carved_suffix}_spec'
+                spectra_task.wrapper.input_filename = f'{{solu}}_{{solv}}_{targstr}_spec_{spectra_task.carved_suffix}_input'
             spectra_task.exc_suffix = f'{targstr}_{meth}{pref(t)}_mldyn'
-            spectra_task.output = f'{{solu}}_{{solv}}_{spectra_task.exc_suffix}_spectrum.png'
+            spectra_task.output = f'{{solu}}_{{solv}}_{spectra_task.exc_suffix}_{spectra_task.carved_suffix}_spectrum.png'
             tdir = '.'
             rslist = list(rand_seed)
             for iw,w in enumerate(get_trajectory_list(ntraj)):
                 rs = rslist[iw]
-                all_trajs.append([f"{tdir}/{{solu}}_{{solv}}_{targstr}_{w}_{meth}{t}{rs}_specdyn_recalc.traj", 
-                                  f"{tdir}/{{solu}}_{{solv}}_{targstrp}_{w}_{meth}{t}{rs}_specdyn_recalc.traj"])
+                all_trajs.append([f"{tdir}/{{solu}}_{{solv}}_{targstr}_{w}_{meth}{t}{rs}_specdyn_recalc_{spectra_task.carved_suffix}.traj", 
+                                  f"{tdir}/{{solu}}_{{solv}}_{targstrp}_{w}_{meth}{t}{rs}_specdyn_recalc_{spectra_task.carved_suffix}.traj"])
                 if corr_traj:
                     all_corr_trajs.append([f"{tdir}/{{solu}}_{{solv}}_{targstr}_{w}_{meth}{t}{rs}_nosolu.traj"])
             spectra_task.trajectory = all_trajs
             spectra_task.correction_trajectory = all_corr_trajs
-            new_spectra_tasks[f'{targstr}_{meth}{t}_specdyn'] = deepcopy(spectra_task)
+            new_spectra_tasks[f'{targstr}_{meth}{t}_specdyn_{spectra_task.carved_suffix}'] = deepcopy(spectra_task)
             
     return new_spectra_tasks
 
